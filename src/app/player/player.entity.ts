7f2a3251a2e382f23a309442c0176ed21ecb10e4
wholzapfel <werner.holzapfel@trivento.nl>
2019-07-30 15:29:33 +0200

    changed player entity

--- src/app/player/player.entity.ts
+++ src/app/player/player.entity.ts
@@ -1,5 +1,4 @@
 import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from 'typeorm';
-import {Teamprediction} from '../team-prediction/team-prediction.entity';
 import {Teamplayer} from '../team-player/teamplayer.entity';
 
 @Entity()
@@ -7,11 +6,43 @@ export class Player {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column('text')
-    firstname: string;
+    @Column('int', {
+        nullable: true
+    })
+    playerReference: number;
 
-    @Column('text', {nullable: true})
-    lastname: string;
+    @Column('int', {
+        nullable: true
+    })
+    teamReference: number;
+
+    @Column('text')
+    team: number;
+
+    @Column('text', {
+        nullable: true
+    })
+    name: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    dateOfBirth: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    countryOfBirth: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    nationality: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    position: string;
 
     @OneToMany(type => Teamplayer, teamplayer => teamplayer.player)
     teamPlayers: Teamplayer[];
